d20d3da84369f5ec5a803cba7fcc326958990b5e
Pierce Morgan <Morgan-P22@ulster.ac.uk>
2025-09-29 14:56:56 +0100

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//This integer variable stores the first number 
+Console.WriteLine("Type in the first number and use the enter key to confirm: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+//This integer variable stores the second number
+Console.WriteLine("Type in the second number and use the enter key to confirm: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//This integer variable stores the sum of firstNumber and secondNumber 
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("The addition of both numbers is {0}", result);
+Console.ReadKey();
